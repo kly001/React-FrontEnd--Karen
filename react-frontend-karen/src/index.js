af44428bb31ad4b5bd260c8721e2a4dfa3d948d4
kly001 <karenly1961@gmail.com>
2019-05-13 16:58:09 -0400

    Rewriting code

--- react-frontend-karen/src/index.js
+++ react-frontend-karen/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from "react-router-dom";
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
 
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-<Router>
-    <App />
-</Router>, 
-document.getElementById('root'));
-
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById("root")
+);
