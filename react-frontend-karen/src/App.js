a58983bdab3510d2e09091589d29cb7a82d70ecc
kly001 <karenly1961@gmail.com>
2019-05-13 19:22:05 -0400

    Setting up Signup Form

--- react-frontend-karen/src/App.js
+++ react-frontend-karen/src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Link, Redirect}  from "react-router-dom";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return(
     <Router>
       <div className="App">
+      <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+
         <div className="App-header ">
           <h1>You Ready To Eat?</h1>
         </div>
-        <Login/>
+     
+      <Route path="/login" component={Login} />
       </div>
-      <p>Not a member? Signup <button>here</button></p>
     </Router>
   )
 }
